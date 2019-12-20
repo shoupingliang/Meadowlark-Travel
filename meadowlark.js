29565355a9950c12d7b7b008311fd59130166ceb
Shou-Ping Liang <shoupingliang@gmail.com>
2019-12-20 09:41:02 +0100

    add unit test

--- meadowlark.js
+++ meadowlark.js
@@ -15,6 +15,12 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
+app.use((req, res, next) => {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
